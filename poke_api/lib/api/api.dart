dc77f50b1aae3a55aa24e0c4218d9df675ee8412
German Bustos <52357914+GermanxD@users.noreply.github.com>
2025-03-13 14:55:15 -0600

    cambio de metodo para obtener la data del pokemon

--- poke_api/lib/api/api.dart
+++ poke_api/lib/api/api.dart
@@ -12,11 +12,46 @@ class PokemonApi {
       final List<dynamic> data = jsonDecode(response.body)['results'];
       return Future.wait(
         data.map((json) async {
-          return await Results.fromUrl(json['name'], json['url']);
+          return await fromUrl(json['name'], json['url']);
         }).toList(),
       );
     } else {
       throw Exception('Failed to load Pokemons');
+    }
+  }
+
+  static Future<Results> fromUrl(String name, String url) async {
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final details = jsonDecode(response.body);
+      return Results(
+        name: name,
+        url: url,
+        imageUrl:
+            details['sprites']['other']['official-artwork']['front_default']
+                as String,
+        id: details['id'] as int,
+        weight: details['weight'] as int,
+        height: details['height'] as int,
+        types:
+            (details['types'] as List?)
+                ?.map((type) => type['type']['name'] as String)
+                .toList() ??
+            [],
+        moves:
+            (details['moves'] as List?)
+                ?.take(2)
+                .map((move) => move['move']['name'] as String)
+                .toList() ??
+            [],
+        stats:
+            (details['stats'] as List?)
+                ?.map((stat) => Stat.fromJson(stat as Map<String, dynamic>))
+                .toList() ??
+            [],
+      );
+    } else {
+      throw Exception('Failed to load Pokemon details');
     }
   }
 }
--- poke_api/lib/models/model_pokemons.dart
+++ poke_api/lib/models/model_pokemons.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
 class Results {
   final String name;
   final String url;
@@ -51,41 +48,6 @@ class Results {
               .toList() ??
           [],
     );
-  }
-
-  static Future<Results> fromUrl(String name, String url) async {
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      final details = jsonDecode(response.body);
-      return Results(
-        name: name,
-        url: url,
-        imageUrl:
-            details['sprites']['other']['official-artwork']['front_default']
-                as String,
-        id: details['id'] as int,
-        weight: details['weight'] as int,
-        height: details['height'] as int,
-        types:
-            (details['types'] as List?)
-                ?.map((type) => type['type']['name'] as String)
-                .toList() ??
-            [],
-        moves:
-            (details['moves'] as List?)
-                ?.take(2)
-                .map((move) => move['move']['name'] as String)
-                .toList() ??
-            [],
-        stats:
-            (details['stats'] as List?)
-                ?.map((stat) => Stat.fromJson(stat as Map<String, dynamic>))
-                .toList() ??
-            [],
-      );
-    } else {
-      throw Exception('Failed to load Pokemon details');
-    }
   }
 }
 
